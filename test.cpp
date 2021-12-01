07f3cb782644889cffa474e54151bc9f7e2f3c54
C.N.PATADIYA <68649029+CNPATADIYA@users.noreply.github.com>
2021-12-01 15:51:57 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,22 @@
+/***** This is/* */cpp 
+program *****/
+
+
+#include<iostream>
+using namespace std;
+
+// Lexical analyser
+
+int main(){
+	int a = 1,b=2.5,c;
+	@
+	while(a){
+		a = a + 1;
+		b = a/c;
+	}
+
+	"\n"
+	printf("%d",a);
+	return 0;
+}
+
